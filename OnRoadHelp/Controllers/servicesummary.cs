454dba04287f1a58682b4347ca2a7152f0e68f93
Ghous Muhammad <ghousmuhammad2016@gmail.com>
2022-10-06 11:00:41 +0500

    OnRoadHelp

---
+++ OnRoadHelp/Controllers/servicesummary.cs
@@ -0,0 +1,11 @@
+﻿namespace OnRoadHelp.Controllers
+{
+    public class servicesummary
+    {
+        public string Service_Type { get; set; }
+        public string FirstName { get; set; }
+
+        public int count { get; set; }
+        public double distance { get; set; }
+    }
+}
---
+++ OnRoadHelp/Models/reqData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnRoadHelp.Models
+{
+    public partial class HelpRequest
+    {
+        public string Service_Type { get; set; }
+        public string FirstName { get; set; }
+
+        public double distance { get; set; }
+    }
+
+    public class NewsAttribshelp
+    {
+       
+    }
+}
